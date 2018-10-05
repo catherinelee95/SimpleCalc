5ca1c99af401a3e1f73317f2bccd4a7308fa01a0
Catherine Lee <catherine598616@gmail.com>
2018-10-05 15:33:31 -0700

    Added initialization files

---
+++ src/main/java/Adder.java
@@ -0,0 +1,14 @@
+
+public class Adder {
+	public int x = 0;
+	public int y = 5;
+	
+	public int getDefaultSum() {
+		return x+y;
+	}
+	
+	public int getSum(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ src/test/java/AdderTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AdderTest {
+	
+	@Test
+	public void testDefault() {
+		Adder adder = new Adder();
+		int sum = adder.getDefaultSum();
+		assertEquals(sum, 5);
+	}
+	
+	@Test
+	public void testSum() {
+		int a = 3;
+		int b = 4;
+		
+		Adder adder = new Adder();
+		int sum = adder.getSum(a,  b);
+		assertEquals(sum, 7);
+	}
+	
+	
+	
+}
